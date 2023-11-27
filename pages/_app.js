4d52b18bfce7001e88e5cb0fe6c699370829ba3d
Elvis Rodrigues <elvisear@hotmail.com>
2023-11-27 11:35:49 -0300

    Jogo implementado

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+import '../TicTacToe.css'
+ 
+// This default export is required in a new `pages/_app.js` file.
+export default function TicTacToe({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,13 @@
-import { useState } from 'react';
-
-function Home() {
-  return (
-        <div>
-            <h1>Contador Personalizado</h1>
-            <Contador />
-        </div>
-  )
-}
-
-function Contador() {
-    const [contador, setContador] = useState(1);
-
-    function adicionarContador(){
-        setContador(contador + 1);
-    }
+import TicTacToe from "./TicTacToe";
+import React from "react";
+function App() {
     return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-
+        <main>
+            <React.StrictMode>
+             <TicTacToe />
+           </React.StrictMode>
+        </main>
     )
 }
 
-export default Home;
+export default App
